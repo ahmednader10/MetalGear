ee3e752b5176610502119efbb8d1ea99176d661e
Nader <ahmednader839@gmail.com>
2016-12-09 19:01:36 +0200

    merging

---
+++ Assets/Scripts/CollectItems.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectItems : MonoBehaviour {
+	private GameObject item;
+	// Use this for initialization
+	void Start () {
+		item = this.gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider player){
+
+		if (player.tag == "Player") {
+			item.SetActive (false);
+			player.gameObject.GetComponent<PlayerScript> ().AddHealthItem (this.gameObject);
+		}
+
+
+	}
+}
