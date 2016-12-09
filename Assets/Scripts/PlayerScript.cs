e72156757e5284e9dc890b68d168d9b6a95b8abd
Mohamed Yasser <mohamed.yasser.abdel.razek@gmail.com>
2016-12-09 18:19:48 +0200

    added health pickup items

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	private ArrayList healthItems;
+	private int health = 100;
+	// Use this for initialization
+	void Start () {
+		healthItems = new ArrayList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddHealthItem(GameObject item) {
+		healthItems.Add (item);
+		print ("ADDED HEALTH: " + item);
+	}
+
+	public void activateHealthItems() {
+		if (healthItems.Count == 0) {
+			return;
+		}
+
+		healthItems.RemoveAt(0);
+
+		if (health >= 100) {
+			return;
+		} else if (health > 50) {
+			health = 100;
+		} else {
+			health += 50;
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/SoldierAIController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+	[RequireComponent(typeof (NavMeshAgent))]
+	[RequireComponent(typeof (ThirdPersonCharacter))]
+	public class SoldierAIController : MonoBehaviour
+	{
+		public NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+		public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+		public Transform[] targets;                                    // target to aim for
+		private Transform activeTarget;
+		private int activeTargetIndex;
+
+		private void Start()
+		{
+			// get the components on the object we need ( should not be null due to require component so no need to check )
+			agent = GetComponentInChildren<NavMeshAgent>();
+			character = GetComponent<ThirdPersonCharacter>();
+			activeTargetIndex = 0;
+			activeTarget = targets [activeTargetIndex];
+
+			if (activeTarget != null)
+				agent.SetDestination(activeTarget.position);
+
+
+			agent.updateRotation = false;
+			agent.updatePosition = true;
+		}
+
+
+		private void Update()
+		{
+		//	if (activeTarget != null)
+		//		agent.SetDestination(activeTarget.position);
+
+		//	print ("Remaining Distance: " + agent.remainingDistance);	
+			if (agent.remainingDistance > agent.stoppingDistance)
+				character.Move (agent.desiredVelocity, false, false);
+			else {
+				nextTarget ();
+				print ("Active Target" + activeTarget);
+			}
+		}
+
+		public void nextTarget() {
+			if (activeTargetIndex == targets.Length - 1) {
+				activeTargetIndex = 0;
+			} else {
+				activeTargetIndex += 1;
+			}
+
+			activeTarget = targets [activeTargetIndex];
+			agent.SetDestination(activeTarget.position);
+		}
+
+		public void SetTarget(Transform target)
+		{
+			this.activeTarget = target;
+		}
+	}
+}
