da4e77dc8d54e5e893a33a058e0dd994ff49d0b1
Hend Hesham Seif <hend.heshamseif@hotmail.com>
2016-12-21 19:14:21 +0200

    Screens

---
+++ Assets/QuitResume.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitResume : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void quitmygame()
+    {
+        Application.Quit();
+        Application.LoadLevel("OptionsMenu");
+
+    }
+
+    public void resumegame()
+
+    {
+        Time.timeScale = 1;
+        /* buttonRes.SetActive(false);
+         buttonQui.SetActive(false);
+         buttonRsm.SetActive(false);*/
+    }
+
+    public void restartgame()
+
+    {
+        Application.LoadLevel("Game");
+    }
+}
---
+++ Assets/SceneManger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManger : MonoBehaviour {
+    public void ChangeToScene(string SceneToChangeTo)
+    {
+        Application.LoadLevel(SceneToChangeTo);
+
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void MuteAll()
+    {
+
+        AudioListener.pause = true;
+        // else
+        //AudioListener.pause = false;
+    }
+}
