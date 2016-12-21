dd39bda7a362201ab5b177a37b7b69a0e970234d
Hend Hesham Seif <hend.heshamseif@hotmail.com>
2016-12-21 22:26:10 +0200

    SoundSystem and Pause Over Game

---
+++ Assets/PauseOverScreen.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseOverScreen : MonoBehaviour {
+    public bool paused;
+    public Canvas menu; // Assign in inspector
+    private bool isShowing;
+    // Use this for initialization
+    void Start () {
+        paused = false;
+        menu.enabled = false;
+  
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+          
+            menu.enabled = true;
+            paused = !paused;
+        }
+        if (paused)
+        {
+            Time.timeScale = 0;
+        }
+        else if (!paused)
+        {
+            Time.timeScale = 1;
+        }
+    }
+    public void quitmygame()
+    {
+        Application.Quit();
+        Application.LoadLevel("OptionsMenu");
+
+    }
+
+    public void resumegame()
+
+    {
+        paused = false;
+        Time.timeScale = 1;
+        menu.enabled = false;
+    }
+
+    public void restartgame()
+
+    {
+        menu.enabled = false;
+        // Application.LoadLevel("Game");
+        Application.Quit();
+    }
+}
